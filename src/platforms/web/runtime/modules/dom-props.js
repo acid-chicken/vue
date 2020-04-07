daddd5d9fe3d222e9f8457fa9dd326bf0c8b329a
Restyled.io <commits@restyled.io>
2020-04-07 21:45:16 +0000

    Restyled by clang-format

--- src/platforms/web/runtime/modules/dom-props.js
+++ src/platforms/web/runtime/modules/dom-props.js
@@ -1,11 +1,11 @@
 /* @flow */
 
-import { isDef, isUndef, extend, toNumber } from 'shared/util'
-import { isSVG } from 'web/util/index'
+import {extend, isDef, isUndef, toNumber} from 'shared/util'
+import {isSVG} from 'web/util/index'
 
 let svgContainer
 
-function updateDOMProps (oldVnode: VNodeWithData, vnode: VNodeWithData) {
+function updateDOMProps(oldVnode: VNodeWithData, vnode: VNodeWithData) {
   if (isUndef(oldVnode.data.domProps) && isUndef(vnode.data.domProps)) {
     return
   }
@@ -26,17 +26,18 @@ function updateDOMProps (oldVnode: VNodeWithData, vnode: VNodeWithData) {
 
   for (key in props) {
     cur = props[key]
-    // ignore children if the node has textContent or innerHTML,
-    // as these will throw away existing DOM nodes and cause removal errors
-    // on subsequent patches (#3360)
-    if (key === 'textContent' || key === 'innerHTML') {
-      if (vnode.children) vnode.children.length = 0
-      if (cur === oldProps[key]) continue
-      // #6601 work around Chrome version <= 55 bug where single textNode
-      // replaced by innerHTML/textContent retains its parentNode property
-      if (elm.childNodes.length === 1) {
-        elm.removeChild(elm.childNodes[0])
-      }
+        // ignore children if the node has textContent or innerHTML,
+        // as these will throw away existing DOM nodes and cause removal errors
+        // on subsequent patches (#3360)
+        if (key === 'textContent' || key === 'innerHTML') {
+      if (vnode.children)
+        vnode.children.length = 0
+        if (cur === oldProps[key]) continue
+            // #6601 work around Chrome version <= 55 bug where single textNode
+            // replaced by innerHTML/textContent retains its parentNode property
+            if (elm.childNodes.length === 1) {
+          elm.removeChild(elm.childNodes[0])
+        }
     }
 
     if (key === 'value' && elm.tagName !== 'PROGRESS') {
@@ -48,7 +49,8 @@ function updateDOMProps (oldVnode: VNodeWithData, vnode: VNodeWithData) {
       if (shouldUpdateValue(elm, strCur)) {
         elm.value = strCur
       }
-    } else if (key === 'innerHTML' && isSVG(elm.tagName) && isUndef(elm.innerHTML)) {
+    } else if (key === 'innerHTML' && isSVG(elm.tagName) &&
+               isUndef(elm.innerHTML)) {
       // IE doesn't support innerHTML for SVG elements
       svgContainer = svgContainer || document.createElement('div')
       svgContainer.innerHTML = `<svg>${cur}</svg>`
@@ -60,57 +62,60 @@ function updateDOMProps (oldVnode: VNodeWithData, vnode: VNodeWithData) {
         elm.appendChild(svg.firstChild)
       }
     } else if (
-      // skip the update if old and new VDOM state is the same.
-      // `value` is handled separately because the DOM value may be temporarily
-      // out of sync with VDOM state due to focus, composition and modifiers.
-      // This  #4521 by skipping the unnecessary `checked` update.
-      cur !== oldProps[key]
-    ) {
+        // skip the update if old and new VDOM state is the same.
+        // `value` is handled separately because the DOM value may be
+        // temporarily out of sync with VDOM state due to focus, composition and
+        // modifiers. This  #4521 by skipping the unnecessary `checked` update.
+        cur !== oldProps[key]) {
       // some property updates can throw
       // e.g. `value` on <progress> w/ non-finite value
       try {
         elm[key] = cur
-      } catch (e) {}
+      } catch (e) {
+      }
     }
   }
 }
 
 // check platforms/web/util/attrs.js acceptValue
-type acceptValueElm = HTMLInputElement | HTMLSelectElement | HTMLOptionElement;
+type acceptValueElm = HTMLInputElement|HTMLSelectElement|HTMLOptionElement;
 
-function shouldUpdateValue (elm: acceptValueElm, checkVal: string): boolean {
-  return (!elm.composing && (
-    elm.tagName === 'OPTION' ||
-    isNotInFocusAndDirty(elm, checkVal) ||
-    isDirtyWithModifiers(elm, checkVal)
-  ))
-}
+function shouldUpdateValue(elm: acceptValueElm, checkVal: string):
+    boolean{return (!elm.composing && (elm.tagName === 'OPTION' ||
+                                       isNotInFocusAndDirty(elm, checkVal) ||
+                                       isDirtyWithModifiers(elm, checkVal)))}
 
-function isNotInFocusAndDirty (elm: acceptValueElm, checkVal: string): boolean {
-  // return true when textbox (.number and .trim) loses focus and its value is
-  // not equal to the updated value
-  let notInFocus = true
-  // #6157
-  // work around IE bug when accessing document.activeElement in an iframe
-  try { notInFocus = document.activeElement !== elm } catch (e) {}
-  return notInFocus && elm.value !== checkVal
-}
-
-function isDirtyWithModifiers (elm: any, newVal: string): boolean {
-  const value = elm.value
-  const modifiers = elm._vModifiers // injected by v-model runtime
-  if (isDef(modifiers)) {
-    if (modifiers.number) {
-      return toNumber(value) !== toNumber(newVal)
+function
+isNotInFocusAndDirty(elm: acceptValueElm, checkVal: string):
+    boolean {
+      // return true when textbox (.number and .trim) loses focus and its value
+      // is not equal to the updated value
+      let notInFocus = true
+      // #6157
+      // work around IE bug when accessing document.activeElement in an iframe
+      try {
+        notInFocus = document.activeElement !== elm
+      } catch (e) {
+      }
+      return notInFocus && elm.value !== checkVal
     }
-    if (modifiers.trim) {
-      return value.trim() !== newVal.trim()
+
+function
+isDirtyWithModifiers(elm: any, newVal: string):
+    boolean {
+      const value = elm.value
+      const modifiers = elm._vModifiers // injected by v-model runtime
+      if (isDef(modifiers)) {
+        if (modifiers.number) {
+          return toNumber(value) !== toNumber(newVal)
+        }
+        if (modifiers.trim) {
+          return value.trim() !== newVal.trim()
+        }
+      }
+      return value !== newVal
     }
-  }
-  return value !== newVal
-}
 
 export default {
-  create: updateDOMProps,
-  update: updateDOMProps
+  create: updateDOMProps, update: updateDOMProps
 }
